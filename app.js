60b12416c85f047808c9e83be67d8087a11478f1
Alex <alx_s@hotmail.fr>
2020-06-26 15:30:48 +0300

    adding filter option

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 
@@ -11,6 +12,9 @@ todoButton.addEventListener("click", addTodo);
 
 // Delete/Check a todo
 todoList.addEventListener("click", deleteCheck);
+
+// Detect filter option
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 
@@ -67,4 +71,35 @@ function deleteCheck(e) {
 		const todo = item.parentElement;
 		todo.classList.toggle("completed");
 	}
+}
+
+function filterTodo(e) {
+	const todos = todoList.childNodes;
+
+	// console.log(todos);
+
+	todos.forEach(function (todo) {
+		switch (e.target.value) {
+			case "all":
+				todo.style.display = "flex";
+				break;
+			case "uncompleted":
+				if (!todo.classList.contains("completed")) {
+					todo.style.display = "flex";
+				} else {
+					todo.style.display = "none";
+				}
+				break;
+			case "completed":
+				if (todo.classList.contains("completed")) {
+					todo.style.display = "flex";
+				} else {
+					todo.style.display = "none";
+				}
+				break;
+
+			default:
+				break;
+		}
+	});
 }
